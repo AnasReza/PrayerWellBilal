c7ddd76297dc092f0c1a5161f8323f64372c414d
Anas Reza <anasreza1@gmail.com>
2024-03-13 14:49:43 +0500

    init with compass

---
+++ android/app/src/main/kotlin/com/prayerwell/prayer_well_uniterrupted/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.prayerwell.prayer_well_uniterrupted
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _deviceSupport = FlutterQiblah.androidDeviceSensorSupport();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xff0c7b93),
+        primaryColorLight: Color(0xff00a8cc),
+        primaryColorDark: Color(0xff27496d),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Color(0xffecce6d)),
+      ),
+      darkTheme:
+      ThemeData.dark().copyWith(colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Color(0xffecce6d))),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: FutureBuilder(
+          future: _deviceSupport,
+          builder: (_, AsyncSnapshot<bool?> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) return Container(width: 100,height: 100,);
+            if (snapshot.hasError)
+              return Center(
+                child: Text("Error: ${snapshot.error.toString()}"),
+              );
+
+            if (snapshot.data!)
+              return QiblahCompass();
+            else
+              return QiblahMaps();
+          },
+        ),
+      ),
+    );
+  }
+}
+
